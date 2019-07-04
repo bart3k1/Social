ca8a56f29bc03b1e9daf7b43b00189788f56ce4e
bart3k1 <bart3k1@gmail.com>
2019-07-04 18:42:12 +0200

    registration and profilr edition

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -119,3 +119,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+#mailing to console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+#media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
